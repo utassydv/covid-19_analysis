59c1a96c1037c7e2aca90b046d8a6aabcfc8a707
David Utassy <utassydv@gmail.com>
2020-11-22 18:23:08 +0100

    chg get_data code

--- codes/get_covid_data.R
+++ codes/get_covid_data.R
@@ -5,15 +5,13 @@
 ##   NO. 1            ##
 ## Get the data       ##
 ########################
-getwd()
-
 # Clear memory and call packages
 rm(list=ls())
 library(WDI)
 library(tidyverse)
 
 # Download COVID cross-sectional data
-date <- '09-11-2020'
+date <- '11-05-2020'
 covid_url <- paste0('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/',
             date,'.csv')
 covid_raw <- read.csv(covid_url)
@@ -23,10 +21,10 @@ pop_raw <- WDI(indicator=c('SP.POP.TOTL'),
                 country="all", start=2019, end=2019)
 
 # Save the raw files
-my_path <- "Documents/Egyetem/CEU/Teaching_2020/Coding_with_R/Codes_4_prep/Assignment_covid/data/"
+my_path <- "/Users/utassydv/Documents/workspaces/CEU/my_repos/covid-19_analysis/"
 # covid data
-write_csv(covid_raw, paste0(my_path,'raw/covid_09_11_2020_raw.csv'))
+write_csv(covid_raw, paste0(my_path,'data/raw/covid_11-05-2020_raw.csv'))
 # population data
-write_csv(pop_raw, paste0(my_path,'raw/pop_WDI_2019.csv'))
+write_csv(pop_raw, paste0(my_path,'data/raw/pop_WDI_2019.csv'))
 
 
